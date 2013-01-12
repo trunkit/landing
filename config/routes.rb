6b3fe6a9ff9d731dd8cae6ba13cf42362e4bb92b
William Weidendorf <wweidendorf@gmail.com>
2013-01-12 15:56:44 -0500

    Added additional promo signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SwaylyLanding::Application.routes.draw do
   # Promo Landing
-  root :to         => "promo#landing"
+  root :to         => "promo#landing", as: :landing
 
   # Promo Steps
-  post "/share"    => "promo#share"
-  post "/signup"   => "promo#signup"
-  post "/complete" => "promo#complete"
+  post "/share"    => "promo#share", as: :share
+  post "/signup"   => "promo#signup", as: :signup
+  post "/complete" => "promo#complete", as: :complete
 end
