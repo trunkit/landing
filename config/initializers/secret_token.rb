ba9151e9f5c6aa06338178989e75bbfdb44cbffe
William Weidendorf <wweidendorf@gmail.com>
2013-01-12 13:03:27 -0500

    Initial application structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwaylyLanding::Application.config.secret_token = '33779de7a74dbb224742ebd667d85475363189dbca013423a0ca6eb3e22b3559fac2c91ef9bf45a2518e7a1b2bfe78d7ca17e0823de44b1f7eea5d01849bcf4f'
