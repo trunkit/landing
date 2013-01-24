d59523f1226eda4e0944369dac321dc381811041
William Weidendorf <wweidendorf@gmail.com>
2013-01-23 23:15:51 -0500

    Added initial content pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ SwaylyLanding::Application.routes.draw do
   post "/share"    => "promo#share", as: :share
   post "/signup"   => "promo#signup", as: :signup
   post "/complete" => "promo#complete", as: :complete
+
+  # Content
+  match "/login"   => "content#login"
+  match "/about"   => "content#about"
 end
