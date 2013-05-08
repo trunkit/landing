36861dcf6e686c2bfb7caa807d0af3192142abc5
William Weidendorf <wweidendorf@gmail.com>
2013-05-07 23:16:22 -0400

    Can now signup for a user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+
+    if @user.errors.blank?
+      session[:user_id] = @user.id
+      redirect_to(brands_path)
+    else
+      render(template: "users/errors", layout: false)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit([:contact_name, :boutique_name, :website, :email, :city, :state, :password, :password_confirmation])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User
   field :website
   field :email
   field :password_digest
+  field :city
   field :state
 
   has_secure_password
+
+  validates_presence_of   :contact_name, :boutique_name, :email
+  validates_uniqueness_of :email
 end
