357286cdbca5645095037bc5be1192f9bedce68b
William Weidendorf <wweidendorf@gmail.com>
2013-05-07 23:44:05 -0400

    Added initial brands lifecycle

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,35 @@
 class BrandsController < ApplicationController
+  before_filter do
+    redirect_to(root_path) unless current_user.present?
+  end
+
+  def index
+    @brands = Brand.all.sort_by{|b| b.votes.size }.reverse
+  end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.create(brand_params)
+
+    if @brand.errors.blank?
+      @brand.toggle_vote(current_user)
+      redirect_to(brands_path)
+    else
+      render(template: "brands/errors", layout: false)
+    end
+  end
+
+  def vote
+    @brand = Brand.find(params[:brand_id])
+    @brand.toggle_vote(current_user)
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit([:name, :email, :website, :phone, :contact_name])
+  end
 end
--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -1,2 +1,11 @@
 module BrandsHelper
+  def vote_button(brand)
+    id = "brand-vote-#{brand.id}"
+
+    if brand.votes.include?(current_user.id)
+      link_to("Voted", brand_vote_path(brand), remote: true, method: :patch, class: "voted", id: id)
+    else
+      link_to("Vote", brand_vote_path(brand), remote: true, method: :patch, class: "vote", id: id)
+    end
+  end
 end
