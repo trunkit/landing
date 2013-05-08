90aee5e8668b6a0ee40d66c3b8bcea1c0f3a1d78
William Weidendorf <wweidendorf@gmail.com>
2013-05-07 23:16:58 -0400

    Don't worry about user logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Landing::Application.routes.draw do
   root to: "content#index"
 
   # User
-  resources :users,    only: [:new, :create]
-  resource  :sessions, only: [:new, :create, :destroy]
+  resources :users,   only: [:new, :create]
+  resource  :session, only: [:new, :create]
 
   # Brands leaderboard
   resources :brands, only: [:index, :new, :create] do
