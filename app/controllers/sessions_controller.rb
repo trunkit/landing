e2fe8381bd401c8e9cb786d1c23fc976674c9d10
William Weidendorf <wweidendorf@gmail.com>
2013-05-07 23:17:34 -0400

    Can now login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
+    user = User.where(email: params[:email]).first
 
-  def destroy
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+    else
+      render(template: "sessions/errors", layout: false)
+    end
   end
 end
